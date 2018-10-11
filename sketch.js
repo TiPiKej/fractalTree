72e33dd959e8fdaf23a9bf6a423ba90f2238df75
TiPiKej <romukkemot@gmail.com>
2018-10-11 20:53:16 +0200

    Updates

--- sketch.js
+++ sketch.js
@@ -1,12 +1,40 @@
-const startingLengthLine = 50;
-const cuttingLength = 5 / 6; // per each requrence
-let degree = 3.14 / (4 + 3);
 const canvasWidth = 600;
 const canvasHeight = 500;
 
+let sliderDegree, sliderLength, sliderCutLen, sliderDensity;
+
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
-  translate(width / 2, height);
 
-  drawLine();
+  createP("Rotate degree");
+  sliderDegree = createSlider(0, PI / 4, PI / 8, PI / 64);
+
+  createP("Starting branch length");
+  sliderLength = createSlider(0, 100, 50);
+
+  createP("Length of cutting branch");
+  sliderCutLen = createSlider(0, 1, 0.8, 0.1);
+
+  createP("Branch density");
+  sliderDensity = createSlider(4, 10, 5);
+}
+
+function draw() {
+  background(120);
+
+  const degree = sliderDegree.value();
+  const lineLength = sliderLength.value();
+  const cuttingLength = sliderCutLen.value();
+  const density = sliderDensity.value();
+
+  const tree = new drawLine({
+    start: createVector(width / 2, height),
+    end: createVector(width / 2, height - lineLength),
+    degree,
+    lineLength,
+    cuttingLength,
+    density
+  });
+
+  tree.draw();
 }
